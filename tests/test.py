5ae2ecbec4f12e727181f8e5b26aedc504169275
Zhihao Lin <lin.zhh99@outlook.com>
2022-02-01 12:28:03 -0800

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -1,6 +1,6 @@
 """Unit test is expected to be here, while I use some usage cases instead."""
 
-from name_analyzer import name_analysis
+from name_analyzer.name_analysis import Name_analysis
 
 if __name__ == '__main__':
     # query = 'Martin Reynolds British'
@@ -10,7 +10,6 @@ if __name__ == '__main__':
     # query = 'Darryl Holliday City Bureau'
     query = 'Yi Fang SCU'
 
-    num, search_face = 2, False
-    obj = name_analysis.Name_analysis(google_dir= '/Users/linzhihao/PycharmProjects/Gender_API/Photo/google/', bing_dir= '/Users/linzhihao/PycharmProjects/Gender_API/Photo/bing/')
-    result = obj.analyze_name(query, 2)
+    obj = Name_analysis(google_dir= 'Your directory for Google images', bing_dir= 'Your directory for Bing images')
+    result = obj.analyze_name(query)
     print(result)
